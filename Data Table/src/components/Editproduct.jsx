a000d73dd60b5b8e00905a24dcd97cb6db315625
Rishivarade <varadehrushikesh87@mail.com>
2024-07-25 10:56:35 +0530

    datatable

--- Data Table/src/components/Editproduct.jsx
+++ Data Table/src/components/Editproduct.jsx
@@ -38,20 +38,20 @@ const Editpage = () => {
         editpro()
     }, [])
     return (
-        <div>
-            <h1>Edit Product</h1>
-            <form action="" onSubmit={(e) => handlesubmit(e)}>
-                <input type="text" placeholder='image' name='image' value={image} onChange={(e) => handlechange(e)} />
-                <input type="text" placeholder='title' name='title' value={title} onChange={(e) => handlechange(e)} />
+        <div style={{paddingBottom:"5%"}}>
+            <h1 style={{textAlign:"center",color:"whitesmoke"}}>Edit Product</h1>
+            <form style={{border:"2px solid black",width:"25%",textAlign:"center",margin:"auto",padding:"10px"}} action="" onSubmit={(e) => handlesubmit(e)}>
+                <input type="text" placeholder='image' name='image' value={image} onChange={(e) => handlechange(e)} /><br/><br/>
+                <input type="text" placeholder='title' name='title' value={title} onChange={(e) => handlechange(e)} /><br/><br/>
                 <select name="category" value={category} onChange={(e) => handlechange(e)} id="">
                     <option value="">Select Your Category </option>
                     <option value="Mens Clothing">Mens Clothing</option>
                     <option value="Jwellery">Jwellery</option>
                     <option value="Electronics">Electronics</option>
                     <option value="Womens Clothing">Womens Clothing</option>
-                </select>
-                <input type="text" placeholder='price' name='price' value={price} onChange={(e) => handlechange(e)} />
-                <input type="text" placeholder='description' name='description' value={description} onChange={(e) => handlechange(e)} />
+                </select><br/><br/>
+                <input type="text" placeholder='price' name='price' value={price} onChange={(e) => handlechange(e)} /><br/><br/>
+                <input type="text" placeholder='description' name='description' value={description} onChange={(e) => handlechange(e)} /><br/><br/>
                 <input type="submit" />
             </form>
 
