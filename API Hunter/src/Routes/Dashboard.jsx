d0a83618be66169ec37c8ba6a84e0bcfc40bad15
Rishivarade <varadehrushikesh87@mail.com>
2024-09-20 11:17:37 +0530

    api

--- API Hunter/src/Routes/Dashboard.jsx
+++ API Hunter/src/Routes/Dashboard.jsx
@@ -1,18 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Authcontext } from "../Context/AuthContext";
 import ProductsTable from "../Components/ProductsTable";
+import Loader from "../Components/Loader";
 
 function Dashboard() {
+  const [data, setdata] = useState([])
   const {user,logout}=useContext(Authcontext)
-  const handlelogout=()=>{
-    logout()   
-  }
+  const [loading, setloading] = useState(true)
+    const getdata = () => {
+        setloading(true)
+        fetch("https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-tech-products")
+            .then(response => response.json())
+            .then(data => {
+                setdata(data.data)
+                setloading(false)
+
+            })
+            .catch((err) => {
+                console.log(err)
+
+            })
+
+    }
+    useEffect(() => {
+        getdata()
+    }, [])
   console.log(user)
   return (
+    loading ? <Loader /> :
     <div>
       <h3>Dashboard</h3>
       <div>
-        <button style={{backgroundColor:"green"}} data-testid="logout-btn" onClick={handlelogout}>Logout</button>
+        <button style={{backgroundColor:"green"}} data-testid="logout-btn" onClick={logout}>Logout</button>
         <p>
           Token:
           <b data-testid="user-token">
@@ -21,7 +40,7 @@ function Dashboard() {
         </p>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
-       <ProductsTable/>
+       <ProductsTable data={data}/>
       </div>
     </div>
   );
--- API Hunter/src/Routes/SingleProductPage.jsx
+++ API Hunter/src/Routes/SingleProductPage.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import Loader from "../Components/Loader"
 
 function SingleProductPage(){
     const[product,setproduct]=useState({})
+    const [loading, setloading] = useState(true)
     const {id}=useParams()
     const getproduct=()=>{
+        setloading(true)
         fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-tech-products/${id}`)
         .then(response=>response.json())
-        .then(data=>setproduct(data.data))
+        .then(data=>{
+            setproduct(data.data)
+            setloading(false)
+        })
         .catch((err)=>console.log(err))
 
     }
@@ -17,6 +23,7 @@ function SingleProductPage(){
     },[])
 
     return (
+        loading ? <Loader /> :
         <div data-testid="products-container" key={product.id}>   
             <div>
                 <h3 data-testid="product-brand">
